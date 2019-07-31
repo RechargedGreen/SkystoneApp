138ee6ee65acc82e9fd96cd46579424bad77476d
David Lukens <davidqcy02@gmail.com>
2019-07-31 11:14:42 -0500

    more rover bot stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverBot/hardware/RoverDumper.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverBot/hardware/RoverDumper.kt
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode.roverBot.hardware
 
+import com.qualcomm.hardware.rev.*
+import com.qualcomm.robotcore.util.*
+import org.firstinspires.ftc.robotcore.external.navigation.*
+import org.firstinspires.ftc.teamcode.lib.*
+
 class RoverDumper {
     private var searchingRight = true
 
@@ -19,6 +24,21 @@ class RoverDumper {
         return false
     }
 
+    private var useLeftCache = false
+    private var useRightCache = false
+    private var leftCache = 0.0
+    private var rightCache = 0.0
+
+    var leftThreshold = 7.0
+    var rightThreshold = 7.0
+    var loadPercent = 0.0
+    var halfPercent = 0.3
+    var dumpPercent = 1.0
+    private val leftSensor = Globals.hMap.get(Rev2mDistanceSensor::class.java, "leftLoading")
+    private val rightSensor = Globals.hMap.get(Rev2mDistanceSensor::class.java, "rightLoading")
+
+    private var hz = 20.0
+
     private var wantToDump = false
 
     fun setDumping(dumping: Boolean) {
@@ -33,6 +53,38 @@ class RoverDumper {
         return false
     }
 
-    fun leftLoaded() = false
-    fun rightLoaded() = false
+    fun leftLoaded(): Boolean {
+        if (!useLeftCache) {
+            leftCache = leftSensor.getDistance(DistanceUnit.INCH)
+            useLeftCache = true
+        }
+        return leftCache < leftThreshold
+    }
+
+    fun rightLoaded(): Boolean {
+        if (!useRightCache) {
+            rightCache = rightSensor.getDistance(DistanceUnit.INCH)
+            useRightCache = true
+        }
+        return rightCache < rightThreshold
+    }
+
+    private val loadingTimer = ElapsedTime()
+
+    fun update() {
+        if (loadingTimer.seconds() > 1.0 / hz) {
+            useLeftCache = false
+            useRightCache = false
+            loadingTimer.reset()
+        }
+
+        if (wantToDump) {
+        } else {
+
+        }
+    }
+
+    private fun setDumpPosition(percent: Double) {
+
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverBot/hardware/RoverIntakeSensors.kt
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.roverBot.hardware
+
+class RoverIntakeSensors {
+    fun loaded() = false
+}
