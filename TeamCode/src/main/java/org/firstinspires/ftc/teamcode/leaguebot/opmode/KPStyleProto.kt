4d3f715ca593838e048cf0d4150e7a5b1d14280a
David Lukens <davidqcy02@gmail.com>
2019-09-13 12:19:42 -0500

    add path mirror tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaguebot/opmode/KPStyleProto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaguebot/opmode/KPStyleProto.kt
@@ -5,13 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.*
 import org.firstinspires.ftc.teamcode.field.*
 import org.firstinspires.ftc.teamcode.leaguebot.*
 import org.firstinspires.ftc.teamcode.lib.*
+import org.firstinspires.ftc.teamcode.lib.RunData.ALLIANCE
 import org.firstinspires.ftc.teamcode.movement.*
-import org.firstinspires.ftc.teamcode.movement.DriveMovement.gamepadControl
-import org.firstinspires.ftc.teamcode.movement.DriveMovement.moveFieldCentric_raw
+import org.firstinspires.ftc.teamcode.movement.DriveMovement.moveFieldCentric_mirror
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.setPosition_raw
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_angle_raw
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_x_raw
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_y_raw
+import org.firstinspires.ftc.teamcode.movement.movementAlgorithms.MovementAlgorithms.PD.goToPosition_mirror
 
 @Config
 @TeleOp
@@ -43,6 +44,12 @@ class KPStyleProto : LeagueBotAutoBase(Alliance.RED, Pose(0.0, 0.0, 0.0)) {
     }
 
     override fun onMainLoop() {
+
+        if (driver.leftBumper.currentState)
+            ALLIANCE = Alliance.BLUE
+        if (driver.rightBumper.currentState)
+            ALLIANCE = Alliance.RED
+
         val turnLeft = turnTarget - world_angle_raw.deg
         val yLeft = yTarget - world_y_raw
         val xLeft = xTarget - world_x_raw
@@ -50,9 +57,14 @@ class KPStyleProto : LeagueBotAutoBase(Alliance.RED, Pose(0.0, 0.0, 0.0)) {
         val speed = Speedometer.fieldSlipPoint
 
         if (move)
+            goToPosition_mirror(24.0, 24.0, 0.0)
+        else
+            moveFieldCentric_mirror(driver.leftStick.x, driver.leftStick.y, driver.rightStick.x)
+
+        /*if (move)
             moveFieldCentric_raw(xLeft * moveP - speed.x * moveD, yLeft * moveP - speed.y * moveD, turnLeft * turnP - Speedometer.degPerSec * turnD)
         else
-            gamepadControl(driver)
+            gamepadControl(driver)*/
 
         if (driver.y.justPressed)
             move = !move
