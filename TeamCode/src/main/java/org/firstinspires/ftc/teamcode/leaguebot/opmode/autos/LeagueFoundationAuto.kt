7dbab8efed736f70ac5b8fdb9d735a6e859ae26d
David Lukens <davidqcy02@gmail.com>
2019-09-14 12:09:07 -0500

    foundation auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaguebot/opmode/autos/LeagueFoundationAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaguebot/opmode/autos/LeagueFoundationAuto.kt
@@ -6,26 +6,27 @@ import org.firstinspires.ftc.teamcode.leaguebot.opmode.hardware.*
 import org.firstinspires.ftc.teamcode.lib.*
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.moveFieldCentric_mirror
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.stopDrive
+import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_x_mirror
+import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_y_mirror
+import org.firstinspires.ftc.teamcode.movement.movementAlgorithms.MovementAlgorithms.PD.goToPosition_mirror
+import org.firstinspires.ftc.teamcode.movement.movementAlgorithms.MovementAlgorithms.PD.pointAngle_mirror
+import kotlin.math.*
 
-abstract class LeagueFoundationAuto(alliance: Alliance) : LeagueBotAutoBase(alliance, Pose(0.0, 0.0, Math.toRadians(-90.0))) {
+abstract class LeagueFoundationAuto(alliance: Alliance) : LeagueBotAutoBase(alliance, Pose(Field.EAST_WALL - LeagueBot.placeLength / 2.0, Field.NORTH_WALL - 24.0 - LeagueBot.placeWidth / 2.0, Math.toRadians(-90.0))) {
     enum class progStates {
         grab,
         pull,
         wait,
         park,
+        completePark,
         doNothing
     }
 
-    var parkAtEnd = true
-
-    val pullSpeed = 1.0
+    val pullSlowSpeed = 0.3
 
     val parkTime = 3.0
 
     override fun onInitLoop() {
-        telemetry.addData("parkingAtEnd: y to toggle", parkAtEnd)
-        if (driver.y.justPressed)
-            parkAtEnd = !parkAtEnd
     }
 
     override fun onMainLoop() {
@@ -33,20 +34,25 @@ abstract class LeagueFoundationAuto(alliance: Alliance) : LeagueBotAutoBase(alli
         telemetry.addData("stage", currentStage)
 
         when (currentStage) {
-            progStates.grab      -> {
-
+            progStates.grab         -> {
+                moveFieldCentric_mirror(-1.0, 0.0, 0.0)
+                pointAngle_mirror(-90.0)
+                if (world_x_mirror < 24.0 + 9.0)
+                    nextStage()
             }
 
-            progStates.pull      -> {
+            progStates.pull         -> {
                 LeagueBot.foundationGrabber.grab()
                 stopDrive()
-                if (stageTimer.seconds() > 0.25)
-                    moveFieldCentric_mirror(pullSpeed, 0.0, 0.0)
+                if (stageTimer.seconds() > 0.25) {
+                    moveFieldCentric_mirror(if (world_x_mirror > Field.EAST_WALL - 9.0 - 6.0) pullSlowSpeed else 1.0, 0.0, 0.0)
+                    pointAngle_mirror(-90.0)
+                }
 
                 timeoutStage(3.0)
             }
 
-            progStates.wait      -> {
+            progStates.wait         -> {
                 stopDrive()
 
                 LeagueBot.foundationGrabber.release()
@@ -54,11 +60,19 @@ abstract class LeagueFoundationAuto(alliance: Alliance) : LeagueBotAutoBase(alli
                     nextStage()
             }
 
-            progStates.park      -> {
+            progStates.park         -> {
+                goToPosition_mirror(Field.EAST_WALL - 20, Field.NORTH_WALL / 2.0, -90.0)
 
+                if ((world_y_mirror - Field.NORTH_WALL / 2.0).absoluteValue < 2.0)
+                    nextStage()
             }
 
-            progStates.doNothing -> {
+            progStates.completePark -> {
+                moveFieldCentric_mirror(0.3, 0.0, 0.0)
+                timeoutStage(1.0)
+            }
+
+            progStates.doNothing    -> {
                 stopDrive()
             }
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/movementAlgorithms/MovementAlgorithms.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/movementAlgorithms/MovementAlgorithms.kt
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.movement.movementAlgorithms
 
-import org.firstinspires.ftc.teamcode.leaguebot.opmode.*
 import org.firstinspires.ftc.teamcode.lib.RunData.ALLIANCE
 import org.firstinspires.ftc.teamcode.movement.*
+import org.firstinspires.ftc.teamcode.movement.DriveMovement.movement_turn
+import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_angle_raw
+import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_x_raw
+import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_y_raw
 
 object MovementAlgorithms {
     lateinit var movementProvider: MovementConstantsProvider
@@ -29,19 +32,28 @@ object MovementAlgorithms {
         var moveD = 0.0
 
         fun goToPosition_raw(x: Double, y: Double, deg: Double) {
-            val turnLeft = KPStyleProto.turnTarget - DriveMovement.world_angle_raw.deg
-            val yLeft = KPStyleProto.yTarget - DriveMovement.world_y_raw
-            val xLeft = KPStyleProto.xTarget - DriveMovement.world_x_raw
+            val turnLeft = deg - world_angle_raw.deg
+            val yLeft = y - world_y_raw
+            val xLeft = x - world_x_raw
 
             val speed = Speedometer.fieldSlipPoint
 
-            val xSpeed = xLeft * KPStyleProto.moveP - speed.x * KPStyleProto.moveD
+            val xSpeed = xLeft * moveP - speed.x * moveD
             val ySpeed = yLeft * moveP - speed.y * moveD
             val turnSpeed = turnLeft * turnP - Speedometer.degPerSec * turnD
             DriveMovement.moveFieldCentric_raw(xSpeed, ySpeed, turnSpeed)
         }
 
         fun goToPosition_mirror(x: Double, y: Double, deg: Double) = goToPosition_raw(x * ALLIANCE.sign, y, deg * ALLIANCE.sign)
+
+        fun pointAngle_raw(deg: Double) {
+            val turnLeft = deg - world_angle_raw.deg
+            movement_turn = turnLeft * turnP - Speedometer.degPerSec * turnD
+        }
+
+        fun pointAngle_mirror(deg: Double) = pointAngle_raw(deg * ALLIANCE.sign)
+
+
     }
 }
 
