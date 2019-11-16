7303eee759aa129660270ffadd0b48db2465c7d0
David Lukens <davidqcy02@gmail.com>
2019-11-15 20:17:00 -0600

    draft 254 motion profiling

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/movementAlgorithms/paths/path/Path.kt
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.movement.movementAlgorithms.paths.path
+
+class Path {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/movementAlgorithms/paths/path/PathFollower.kt
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.movement.movementAlgorithms.paths.path
+
+/**
+ * Created by David Lukens on 11/15/2019.
+ */
+class PathFollower {
+}
