4db46639f0f6c1c94f03ad73de3358b410466bb5
David Lukens <davidqcy02@gmail.com>
2019-11-20 09:51:25 -0600

    Update LittleFanciesSkystoneFoundationPark.kt
    things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaguebot/opmode/autos/LittleFanciesSkystoneFoundationPark.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/leaguebot/opmode/autos/LittleFanciesSkystoneFoundationPark.kt
@@ -6,10 +6,14 @@ import org.firstinspires.ftc.teamcode.field.Pose
 import org.firstinspires.ftc.teamcode.field.Quarry
 import org.firstinspires.ftc.teamcode.leaguebot.LeagueBotAutoBase
 import org.firstinspires.ftc.teamcode.leaguebot.opmode.hardware.LeagueBot
+import org.firstinspires.ftc.teamcode.leaguebot.opmode.hardware.MainIntake
 import org.firstinspires.ftc.teamcode.lib.Alliance
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.moveFieldCentric_mirror
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.stopDrive
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_x_mirror
+import org.firstinspires.ftc.teamcode.movement.DriveMovement.world_y_mirror
+import org.firstinspires.ftc.teamcode.movement.movementAlgorithms.MovementAlgorithms.PD.goToPosition_mirror
+import org.firstinspires.ftc.teamcode.movement.movementAlgorithms.MovementAlgorithms.PD.pointAngle_mirror
 import org.firstinspires.ftc.teamcode.movement.toRadians
 
 abstract class LittleFanciesSkystoneFoundationPark(alliance: Alliance) : LeagueBotAutoBase(alliance, Pose(Field.EAST_WALL - 9.0, -24.0 - 9.0, (-90.0).toRadians)) {
@@ -23,6 +27,10 @@ abstract class LittleFanciesSkystoneFoundationPark(alliance: Alliance) : LeagueB
     }
 
     var stoneX = 0.0
+    var distanceFromStone = 3.0
+    var distanceFromNextStone = 3.0
+
+    val halfStoneWidth = 4.0
 
     override fun onStart() {
         stoneX = Quarry.popStone().center_x
@@ -33,22 +41,32 @@ abstract class LittleFanciesSkystoneFoundationPark(alliance: Alliance) : LeagueB
 
         stopDrive()
         LeagueBot.foundationGrabber.release()
+        LeagueBot.intake.state = MainIntake.State.STOP
 
         when (currentStage) {
             progStages.gettingPastPartner -> {
                 moveFieldCentric_mirror(-0.5, 0.0, 0.0)
-                if(world_x_mirror < 72.0 - 18.0 - 9.0)
+                if (world_x_mirror < 72.0 - 18.0 - 9.0)
                     nextStage()
             }
             progStages.lineUpParrallel -> {
-
+                val r = goToPosition_mirror(36.0, stoneX + halfStoneWidth + distanceFromStone, 180.0)
+                if (r.point.hypot < 2.0 && r.deg < 2.0)
+                    nextStage()
             }
             progStages.driveIntoQuarry -> {
-
+                val r = goToPosition_mirror(22.0, stoneX - halfStoneWidth + distanceFromStone, 180.0)
+                if (r.point.hypot < 2.0 && r.deg < 2.0)
+                    nextStage()
             }
 
             progStages.intake -> {
-
+                moveFieldCentric_mirror(0.0, 0.25, 0.0)
+                pointAngle_mirror(180.0)
+                if (world_y_mirror < stoneX + 9.0 - 2.0 + distanceFromNextStone)
+                    stopDrive()
+                if (isTimedOut(2.0))
+                    nextStage()
             }
 
             progStages.doNothing -> {
