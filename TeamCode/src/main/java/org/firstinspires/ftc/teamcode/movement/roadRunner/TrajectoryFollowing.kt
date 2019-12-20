bdd26a43d213536c84b264c0c934f3f0716e4158
David Lukens <davidqcy02@gmail.com>
2019-12-20 15:37:08 -0600

    add graphing to TrajectoryFollowing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/roadRunner/TrajectoryFollowing.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/roadRunner/TrajectoryFollowing.kt
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.movement.roadRunner
 
 import com.acmerobotics.roadrunner.followers.*
 import com.acmerobotics.roadrunner.trajectory.*
+import org.firstinspires.ftc.teamcode.movement.*
 import org.firstinspires.ftc.teamcode.movement.DriveMovement.roadRunnerPose2dRaw
 import org.firstinspires.ftc.teamcode.movement.roadRunner.RoadRunnerConstraints.axis_pid
 import org.firstinspires.ftc.teamcode.movement.roadRunner.RoadRunnerConstraints.heading_pid
+import org.firstinspires.ftc.teamcode.opmodeLib.Globals.mode
 
 object TrajectoryFollowing {
     val follower = HolonomicPIDVAFollower(axis_pid, axis_pid, heading_pid)
@@ -19,6 +21,12 @@ object TrajectoryFollowing {
         val isFollowing = follower.isFollowing()
 
         RoadRunnerConstraints.setVelocity(signal.vel)
+
+        val error = follower.lastError
+        mode.combinedPacket.put("xError", error.x)
+        mode.combinedPacket.put("yError", error.x)
+        mode.combinedPacket.put("radError", error.heading)
+        mode.combinedPacket.put("degError", error.heading.toDegrees)
 
         return !isFollowing
     }
